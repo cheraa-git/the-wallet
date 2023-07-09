11de90dc3822ec2b046df4f7b638f3d6ace08426
cheraa-git <chernetsov.aa@mail.ru>
2023-07-09 21:59:08 +0400

    server: initialisation

---
+++ server/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import config from 'config'
+import chalk from 'chalk'
+import dotenv from 'dotenv'
+import * as mongoose from 'mongoose'
+import routes from './routes'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = config.get('port') ?? 8080
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api', routes)
+
+async function start() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || '')
+    console.log(chalk.green('MongoDB connected'))
+    app.listen(PORT, () => {
+      console.log(chalk.green(`Server has been started on port ${PORT}...`))
+    })
+  } catch (error: any) {
+    console.log(chalk.red(error.message))
+    process.exit(1)
+  }
+
+}
+
+start()
